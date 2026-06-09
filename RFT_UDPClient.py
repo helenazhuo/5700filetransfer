74fe058341922a15e06df43be1bf8946ed3cde48
Helena Zhuo <zhuo.h@northeastern.edu>
2026-06-08 20:46:19 -0700

    Stubbed longer fn, implemented basic fns

--- RFT_UDPClient.py
+++ RFT_UDPClient.py
@@ -1,8 +1,23 @@
+from socket import socket, timeout, AF_INET, SOCK_RAW
+
 class RFT_UDPClient:
-     """
-     Takes a filename as input to send to a UDP web server to request a download of that file.
-     Uses SOCK_RAW as the type of socket.
+    """Requests download of a file from a UDP web server.
 
     Attributes:
         fn (str): The filename.
     """
+     
+    def __init__(self, clientID):
+        id = clientID
+    
+    def reqFn(self, fn, serverName, serverPort):
+        message = f'REQ {fn}' # TODO: construct msg with RFT_UDPPacket.py
+        clientSocket = socket(AF_INET, SOCK_RAW)
+        clientSocket.settimeout(1.0)
+        self.clientSocket.sendto(message.encode(), (serverName, serverPort))
+        try: 
+            reply, serverAddress = clientSocket.recvfrom(1024)
+            print('Response:', reply)
+        except timeout as e:
+            f'Error: request timed out.'
+        clientSocket.close()
--- RFT_UDPServer.py
+++ RFT_UDPServer.py
@@ -1,8 +1,32 @@
+from socket import socket, AF_INET, SOCK_RAW
+initIP = ''
+initPort = 12000
+
 class RFT_UDPServer:
-     """
-     Given a filename, this server produces a text output file that reports the results of the downloaded file.
-     Error control mechanisms included: Checksum, Sequence Numbers, Cumulative Ack #s, Retransmission due to timeout.
+    """Outputs a text file that reports the results of a downloaded file.
 
     Attributes:
         fn (str): The filename.
     """
+
+    # Start with one socket
+    def __init__(self):
+        sockets = [self.addSocket(initIP, initPort)]
+
+    def addSocket(self, IPaddress, portNum)->socket:
+        ip = IPaddress
+        port = portNum
+        serverSocket = socket(AF_INET, SOCK_RAW)
+        return serverSocket
+
+    def startServer(self):
+        initSocket = self.sockets[0]
+        msg, address = initSocket.recvfrom(1024)
+
+        # Response
+        response = 'getFile' # TODO: implement. Open new sockets if neccessary
+        initSocket.sendto(response, address)
+        
+    def reportToTxt(self)->bool:
+        # TODO: implement
+        return True
